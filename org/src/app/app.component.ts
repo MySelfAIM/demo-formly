e25a116ab9d73cb01f5ee11c90db92e9eef6f73c
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-03-11 22:29:12 +0100

    add initial config for formly demo

--- org/src/app/app.component.ts
+++ org/src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { FormlyMaterialModule } from '@ngx-formly/material';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    FormlyModule,
+    FormlyMaterialModule,
+  ],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'org';
+
+  form = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      },
+    },
+  ];
+
+  onSubmit(model: any) {
+    console.log(model);
+  }
 }
--- org/src/app/app.config.ts
+++ org/src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { FormlyModule } from '@ngx-formly/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideAnimations(),
+    importProvidersFrom(FormlyModule.forRoot()),
+  ],
 };
