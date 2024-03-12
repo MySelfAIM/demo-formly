06d2aae5b89c1eb047ea4dfcefcddc2f637d3367
Alexandre Insua <92730122+gbtec-ext-alexandreinsua@users.noreply.github.com>
2024-03-12 08:35:36 +0100

    add validation examples

--- org/src/app/app.component.ts
+++ org/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
@@ -29,6 +33,48 @@ export class AppComponent {
         label: 'Email address',
         placeholder: 'Enter email',
         required: true,
+      },
+    },
+    {
+      key: 'ip',
+      type: 'input',
+      props: {
+        label:
+          'IP Address (using custom validation through `validators.expression` property)',
+        description:
+          'custom validation message through `validators.expression` property',
+        required: true,
+      },
+      validators: {
+        ip: {
+          expression: (c: AbstractControl) =>
+            /(\d{1,3}\.){3}\d{1,3}/.test(c.value),
+          message: (error: any, field: FormlyFieldConfig) =>
+            `"${field?.formControl?.value}" is not a valid IP Address`,
+        },
+      },
+    },
+    {
+      key: 'ip_async',
+      type: 'input',
+      props: {
+        label:
+          'IP Address (using custom validation through `validators.expression` property)',
+        description:
+          'custom validation message through `validators.expression` property',
+        required: true,
+      },
+      asyncValidators: {
+        ip: {
+          expression: (c: AbstractControl) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                resolve(/(\d{1,3}\.){3}\d{1,3}/.test(c.value));
+              }, 1000);
+            }),
+          message: (error: any, field: FormlyFieldConfig) =>
+            `"${field?.formControl?.value}" is not a valid IP Address`,
+        },
       },
     },
   ];
